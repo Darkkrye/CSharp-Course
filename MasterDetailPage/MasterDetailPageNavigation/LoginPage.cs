0d49e7e0e3f5753cdf2511dedff235a36394d5d2
Darkkrye <pierre.boudon.27@gmail.com>
2017-01-07 12:09:01 +0100

    Added beginning of MasterDetailPage.

---
+++ MasterDetailPage/MasterDetailPageNavigation/LoginPage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace MasterDetailPageNavigation
+{
+	public class LoginPage : ContentView
+	{
+		public LoginPage()
+		{
+			Content = new Label { Text = "Hello ContentView" };
+		}
+	}
+}
+
