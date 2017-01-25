9e129ba991e1a919c63adccd51fa9f73715b92f0
Darkkrye <pierre.boudon.27@gmail.com>
2017-01-25 20:08:31 +0100

    Added new TPs.

---
+++ App2/App2/App2/App.cs
@@ -0,0 +1,70 @@
+﻿using Plugin.Connectivity;
+using Plugin.Connectivity.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace App2
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            /*var content = new ContentPage
+            {
+                Title = "App2",
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = {
+                        new Label {
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            Text = "Welcome to Xamarin Forms!"
+                        }
+                    }
+                }
+            };*/
+            var content = new NetworkViewPage
+            {
+                Title = "MyApp"
+            };
+
+            MainPage = new NavigationPage(content);
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+            base.OnStart();
+            System.Diagnostics.Debug.WriteLine("Passed");
+            CrossConnectivity.Current.ConnectivityChanged += HandleConnectivityChanged;
+        }
+
+        void HandleConnectivityChanged(object send, ConnectivityChangedEventArgs e)
+        {
+            Type currentPage = this.MainPage.GetType();
+
+            if (e.IsConnected && currentPage != typeof(NetworkViewPage))
+            {
+                this.MainPage = new NetworkViewPage();
+            } else if (!e.IsConnected && currentPage != typeof(NoNetworkPage))
+            {
+                this.MainPage = new NoNetworkPage();
+            }
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ App2/App2/App2/NetworkViewPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.Connectivity;
+
+using Xamarin.Forms;
+using Plugin.Connectivity.Abstractions;
+
+namespace App2
+{
+    public partial class NetworkViewPage : ContentPage
+    {
+        public NetworkViewPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            ConnectionDetails.Text = CrossConnectivity.Current.ConnectionTypes.First().ToString();
+            CrossConnectivity.Current.ConnectivityChanged += UpdateNetworkInfo;
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            CrossConnectivity.Current.ConnectivityChanged -= UpdateNetworkInfo;
+        }
+
+        private void UpdateNetworkInfo(object sender, ConnectivityChangedEventArgs e)
+        {
+            if (CrossConnectivity.Current != null && CrossConnectivity.Current.ConnectionTypes != null)
+            {
+                var connectionType = CrossConnectivity.Current.ConnectionTypes.FirstOrDefault();
+                ConnectionDetails.Text = connectionType.ToString();
+            }
+        }
+    }
+}
